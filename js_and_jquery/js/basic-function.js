80f0dd844a9f042b8ebc7c605c408c39a5276f1d
Felipe Adrian Lopez <pon.felipon@gmail.com>
2015-05-19 11:53:38 -0700

    arreglando un typo en el codigo js

--- js_and_jquery/js/basic-function.js
+++ js_and_jquery/js/basic-function.js
@@ -1,6 +1,6 @@
 var msg = 'Inscribete a nuestra lista y recibe 10% de descuento!';
 function updateMessage(){
-    var el = document.getElementyById('mensaje');
+    var el = document.getElementById('mensaje');
     el.textContent = msg;
 }
 updateMessage();
