1ae6dd5c6c6d44e043834a60e4c1af1525ea889a
Felipe Adrian Lopez <pon.felipon@gmail.com>
2015-05-19 06:21:24 -0700

    quitando los archivos que no sirven

--- holamundo.js
+++
@@ -1,9 +0,0 @@
-if (typeof console === 'object'){
-    console.log('Hola desde consola');
-}
-else if(typeof document === 'object'){
-    document.write('Hola desde documento')
-}
-else{
-    print('hola mundo')
-}
--- mult3y5.js
+++
@@ -1,5 +0,0 @@
-var user = prompt('Cual es su nombre');
-var valor = prompt('limite');
-
-print('primero que nada, hola: ', user);
-
