dfaf2c05ed004369c87110d6994e60c8b468dc9e
Felipe Adrian Lopez <pon.felipon@gmail.com>
2015-05-19 11:47:24 -0700

    Agregando una pagina con js de ejemplo, ejemplo de JavaScript and Jquery interactive front end

---
+++ js_and_jquery/js/basic-function.js
@@ -0,0 +1,7 @@
+var msg = 'Inscribete a nuestra lista y recibe 10% de descuento!';
+function updateMessage(){
+    var el = document.getElementyById('mensaje');
+    el.textContent = msg;
+}
+updateMessage();
+
