568d6ac881e15cb556810aa660f2326f53c8257a
ponkbrown <pon.felipon@gmail.com>
2015-05-23 15:46:31 -0700

    jugando con jquery

--- sandbox/script.js
+++ sandbox/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
    $('div').mouseenter(function() {
        $(this).animate({
-           height: '+=70px'
+           height: '+=70px';
+           width: '+=70px';
        });
    });
    $('div').mouseleave(function() {
        $(this).animate({
            height: '-=70px'
+
        }); 
    });
    $('div').click(function() {
